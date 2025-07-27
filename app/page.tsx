3c36239edd4bdc93cc6e88cd61dd4da8e356f197
prashxant <notxdead0@gmail.com>
2025-07-27 18:44:18 +0530

    haaahh

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Container } from "./components/container";
 import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
@@ -15,6 +16,15 @@ export default function Home() {
     <Navbar/>
     <Hero/>
   </Container>
+  <div className="relative w-full">
+    <div className="absolute inset-x-0 left-0 h-px w-full  bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none z-0"/>
+    <div className="max-w-6xl  mx-auto p-4">
+    <Image src="/hh.webp" alt="banner image" width={1000} height={1000} className="rouded-2xl object-cover
+    w-full object-left-top border-neutral-200 shadow-md mask-b-from-0% to-100%">
+
+    </Image>
+</div>
   </div>
-  );
+
+  </div>);
 }
