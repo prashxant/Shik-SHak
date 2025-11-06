7834b9b9a0f66f85ed0201afabdfadbdb324e1f2
prashxant <notxdead0@gmail.com>
2025-11-06 19:42:51 +0530

    full hero section done

--- app/component/hero.tsx
+++ app/component/hero.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 
 
  export const Hero = () => {
@@ -16,6 +17,9 @@
         <div className="flex items-center gap-4 mt-10">
            <button className='bg-[#2579F4] cursor-pointer px-4 py-2 rounded-lg text-white font-bold shadow-lg text-shadow-md tracking-wide'>Get started</button>
              <button className='px-4 py-2 cursor-pointer rounded-lg text-black   text-shadow-md tracking-wide'>Pricing &rarr;</button>
+        </div>
+        <div className=" max-w-7xl mx-auto mt-30">
+            <Image className="rounded-xl w-full object-cover object-left-top border-neutral-200 shadow-md mask-b-from-0% to-100%"src='/hh.webp' alt="dashboard" height={1000} width={1500}/>
         </div>
 
     </div>
