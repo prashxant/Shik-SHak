867be982832d08f7cbff28b933ec4c1f30694293
prashxant <notxdead0@gmail.com>
2025-12-15 16:46:14 +0530

    y

--- app/component/kybr/keyboard.tsx
+++ app/component/kybr/keyboard.tsx
@@ -1,12 +1,7 @@
 'use client'
 import { keys } from "@/lib/keys";
 
-
-
-
-
  export const Keyboard = () => {
-
 
 
  const baseStyle ="m-0.5 flex text-md items-center justify-center size-16 border-2 border-neutral-700 rounded-[10px] bg-[#0A090D] text-white shadow-slate-200 shadow-lg/50 hover:shadow-none cursor-pointer transition duration-200 hover:scale-95";
