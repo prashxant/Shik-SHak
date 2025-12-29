7b5c45d32b15827dc742723b4107471f76097c52
prashxant <notxdead0@gmail.com>
2025-12-29 15:15:56 +0530

    prefetch

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <div className="flex flex-col gap-10 pt-10">
           {Sites.map((site, idx) => (
             <div className="transition duration-200 hover:scale-110" key={idx}>
-              <Link
+              <Link prefetch
                 className="rounded-md border-2 border-neutral-700 px-4 py-2 text-4xl font-semibold text-neutral-600 shadow-slate-500/80 transition duration-300 hover:text-neutral-200 hover:shadow-2xl"
                 href={site.herf}
               >
